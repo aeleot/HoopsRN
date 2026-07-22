9f123fb27d2ab3afce1fa4969dcf130c7654647a
Elliot <elliot.thapaliya@gmail.com>
2026-07-21 21:49:28 -0400

    Initial Commit

---
+++ hoopr/hooprApp.swift
@@ -0,0 +1,32 @@
+//
+//  hooprApp.swift
+//  hoopr
+//
+//  Created by Elliot Thapaliya on 7/21/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct hooprApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
